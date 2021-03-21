6d39b30b22c84ac70be8f4ef4bb0d82eedd1248f
user <neetamalik07@gmail.com>
2021-03-21 17:21:32 +0530

    commit in remote server after cloning now

---
+++ cppfile.cpp
@@ -0,0 +1,8 @@
+\\this is a cpp file i created to just test what the git do in this case\\
+#include<iostream>
+int main()
+{
+   std::cout<<"testing git";
+   return 0;
+}
+\\this line is to edit this file to see how git gui handle this\\
